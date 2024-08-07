56428b70e6d96b5c054ada2c71cb63c2c1caa453
callumhume <75507869+callumhume@users.noreply.github.com>
2024-08-06 22:20:30 -0400

    Initial code commit
    Working: Settings selection (COM port, baud rate, GPS fix rate)

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace GPSSimulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox_COMSelector = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox_BaudSelector = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox_FixRateSelector = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBox_COMSelector
+            // 
+            this.comboBox_COMSelector.FormattingEnabled = true;
+            this.comboBox_COMSelector.Location = new System.Drawing.Point(71, 417);
+            this.comboBox_COMSelector.Name = "comboBox_COMSelector";
+            this.comboBox_COMSelector.Size = new System.Drawing.Size(67, 21);
+            this.comboBox_COMSelector.TabIndex = 0;
+            this.comboBox_COMSelector.SelectedIndexChanged += new System.EventHandler(this.comboBox_COMSelector_SelectedIndexChanged);
+            this.comboBox_COMSelector.TextUpdate += new System.EventHandler(this.comboBox_COMSelector_TextUpdate);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 420);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 420);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Baud Rate";
+            // 
+            // comboBox_BaudSelector
+            // 
+            this.comboBox_BaudSelector.FormattingEnabled = true;
+            this.comboBox_BaudSelector.Location = new System.Drawing.Point(227, 417);
+            this.comboBox_BaudSelector.Name = "comboBox_BaudSelector";
+            this.comboBox_BaudSelector.Size = new System.Drawing.Size(82, 21);
+            this.comboBox_BaudSelector.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(332, 420);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Fix Rate (Hz)";
+            // 
+            // comboBox_FixRateSelector
+            // 
+            this.comboBox_FixRateSelector.FormattingEnabled = true;
+            this.comboBox_FixRateSelector.Location = new System.Drawing.Point(406, 417);
+            this.comboBox_FixRateSelector.Name = "comboBox_FixRateSelector";
+            this.comboBox_FixRateSelector.Size = new System.Drawing.Size(64, 21);
+            this.comboBox_FixRateSelector.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox_FixRateSelector);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox_BaudSelector);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox_COMSelector);
+            this.Name = "Form1";
+            this.Text = "GPS Simulator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox_COMSelector;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox_BaudSelector;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox_FixRateSelector;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace GPSSimulator
+{
+    public partial class Form1 : Form
+    {
+
+        string selectedCOMPort = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            populateComboBoxLists();
+
+            // TODO: Select previous settings
+
+            // TODO: Create GPS logic
+
+            // TODO: Create output logic (autoconnect at beginning)
+
+            // TODO: Create position control logic
+        }
+
+        private void populateComboBoxLists()
+        {
+            string[] COMPorts = SerialPort.GetPortNames();
+
+            for (int i = 0; i < COMPorts.Length; i++)
+            {
+                Console.WriteLine(COMPorts[i]); // TODO: Only print out with some debug mode?
+                comboBox_COMSelector.Items.Add(COMPorts[i]);
+            }
+
+            comboBox_COMSelector.SelectedIndex = 0;
+            comboBox_COMSelector.Refresh();
+
+
+            int[] baudRates = { //110,
+                                //300,
+                                //600,
+                                //1200,
+                                //2400,
+                                //4800,
+                                9600,
+                                //14400,
+                                19200,
+                                38400,
+                                //57600,
+                                115200,
+                                //230400,
+                                //460800,
+                                //921600
+                                };
+
+            for (int i = 0; i < baudRates.Length; i++)
+            {
+                Console.WriteLine(baudRates[i].ToString());
+                comboBox_BaudSelector.Items.Add(baudRates[i]);
+            }
+
+            comboBox_BaudSelector.SelectedIndex = 0;
+            comboBox_BaudSelector.Refresh();
+
+
+            int[] fixRates = { 1, 2, 4, 5, 10, 20 };
+
+            for (int i = 0; i < fixRates.Length; i++)
+            {
+                Console.WriteLine(fixRates[i].ToString());
+                comboBox_FixRateSelector.Items.Add(fixRates[i]);
+            }
+
+            comboBox_FixRateSelector.SelectedIndex = 0;
+            comboBox_FixRateSelector.Refresh();
+        }
+
+        private void comboBox_COMSelector_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            selectedCOMPort = comboBox_COMSelector.SelectedItem.ToString();
+        }
+
+        private void comboBox_COMSelector_TextUpdate(object sender, EventArgs e)
+        {
+            selectedCOMPort = comboBox_COMSelector.Text;
+        }
+    }
+}
